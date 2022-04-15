6e6bf0d16388d7f008452e9e4bd4622cdb491766
monde1999 <monchingdesierto49@gmail.com>
2022-04-15 12:18:23 +0800

    added teardown()

--- Scenario1.py
+++ Scenario1.py
@@ -1,66 +1,20 @@
-
-""""
-import unittest
-from borrower import Borrower
-from loan import Loan
-
-class TestScenario1(unittest.TestCase):
-  def setUp(self) -> None:
-    loanAmount = 100000
-    borrower = Borrower(
-      depositAmount=loanAmount, nationality='Filipino', age=18,
-      employment='employed', monthlyIncome=30000, accountNumber='0123456'
-    )
-    self.loan = Loan(loanAmount=loanAmount, borrower=borrower)
-    return super().setUp()
-  def tearDown(self) -> None:
-    # delete all the temporary data created in setUp() method
-    return super().tearDown()
-  def test_nationality(self):
-    c1 = self.loan.checkNationality()
-    self.assertEqual(c1, True)
-  def test_age(self):
-    c2 = self.loan.checkAge()
-    self.assertEqual(c2, True)
-  def test_employment(self):
-    c3 = self.loan.checkEmployment()
-    self.assertEqual(c3, True)
-  def test_monthly_income(self):
-    c4 = self.loan.checkMonthlyIncome()
-    self.assertEqual(c4, True)
-  def test_loan_amount(self):
-    c5 = self.loan.checkLoanAmount()
-    self.assertEqual(c5, True)
-  def test_submit_bank_account(self):
-    a1 = self.loan.submitBankAccount()
-    self.assertEqual(a1, True)
-  def test_grant(self):
-    a2 = self.loan.grant()
-    self.assertEqual(a2, True)
-  def test_deposit(self):
-    a3 = self.loan.deposit()
-    self.assertEqual(a3, True)
-if __name__ == '__main__':
-    unittest.main()
-"""
-
-
 import unittest
 from Database import Database, Student
 from Enroll import Enroll
 class Scenario1(unittest.TestCase):
-    def setUp(self) -> None:
+    def setUp(self):
         self.DB = Database()
         self.student = Student(first_name='Mark', last_name='Simbajon')
         self.enroll = Enroll(self.student)
-
         self.enroll.set_complete_prerequisite_courses(True)
         self.enroll.set_submit_documents(True)
         self.enroll.set_has_passed_entrance_exam(True)
         self.enroll.set_pay_fee(True)
         self.enroll.enroll(self.DB)
-
         return super().setUp()
+
+    def tearDown(self):
+        return super().tearDown()
 
     def test_set_completed_prerequisite_courses(self):
         c1 = self.enroll.has_completed_prerequisite_senior_high_courses
@@ -102,5 +56,3 @@ class Scenario1(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-
-##unsaon pag sulod? murag naa sa ubos... 
